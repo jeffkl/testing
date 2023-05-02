7430c9233569c0051c47f68c334c6647f509939c
Jeff Kluge <jeffkl@microsoft.com>
2023-05-02 14:18:32 -0400

    WIP

---
+++ src/ClassLibrary1.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ClassLibrary1.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/ClassLibrary2.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ClassLibrary2.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
